8f9828016aad906631e0c99f6c764c49ec61c19b
federico urbani <urbanifederico5@gmail.com>
2023-05-14 14:36:22 +0200

    updated main

---
+++ src/main/java/com/develhope/websocket1/entity/MessageDTO.java
@@ -0,0 +1,29 @@
+package com.develhope.websocket1.entity;
+
+public class MessageDTO {
+
+    private String message;
+    private String type;
+
+    public MessageDTO(){}
+    public MessageDTO(String message, String type) {
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
